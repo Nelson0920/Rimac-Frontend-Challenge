c91c2a58ec4d176f91993f37d1819c3a931c27cf
Nelson0920 <nelson.guerrero25.ng@gmail.com>
2025-08-12 18:30:27 -0400

    error auth

--- src/context/auth/publicRoute.tsx
+++ src/context/auth/publicRoute.tsx
@@ -1,22 +1,9 @@
-import React, { useEffect, useRef } from "react";
-import { Navigate, Outlet, useLocation } from "react-router-dom";
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "./AuthContext";
 
 const PublicRoute: React.FC = () => {
-  const { isAuthenticated, logout } = useAuth();
-  const location = useLocation();
-  const prevPath = useRef<string>("");
-
-  useEffect(() => {
-    if (
-      isAuthenticated &&
-      location.pathname === "/" &&
-      prevPath.current !== "/"
-    ) {
-      logout();
-    }
-    prevPath.current = location.pathname;
-  }, [isAuthenticated, location.pathname, logout]);
+  const { isAuthenticated } = useAuth();
 
   return !isAuthenticated ? <Outlet /> : <Navigate to="/home" replace />;
 };
