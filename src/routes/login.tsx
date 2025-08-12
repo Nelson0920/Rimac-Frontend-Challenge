1e6f9f72aadefaf7f2293910bc3bdff2191028b3
Nelson0920 <nelson.guerrero25.ng@gmail.com>
2025-08-12 15:17:28 -0400

    background login

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,33 +1,40 @@
-import { useState } from "react"
+import { useState } from "react";
 import {
   Button,
   Input,
-} from "../components/common"
+} from "../components/common";
+import "../styles/components/loginPage.scss";
 
 export default function Login() {
-  const [phone, setPhone] = useState("")
+  const [phone, setPhone] = useState("");
 
   const handleInputChange = (value: string) => {
-    setPhone(value)
-    console.log("Telefono:", value)
-  }
+    setPhone(value);
+    console.log("Telefono:", value);
+  };
 
   return (
-    <div className="w-full p-4">
-      <h1>Login</h1>
-      <Button label="Cotiza aquí" onClick={() => alert('Click')} />
-      
-      <Input
-        type="tel"
-        placeholder="Celular"
-        value={phone}
-        onChange={handleInputChange}
-      >
-        <Input.Label />
-        <Input.Field pattern="[0-9]{7,12}" />
-      </Input>
+    <div>
+      <div className="absolute inset-0 -z-10 flex containerBackground">
+        <div className={`w-1/2 h-full circleLeft`} />
+        <div className={`w-1/2 h-full circleRight`} />
+      </div>
 
-      <p>Telefono: {phone}</p>
+      <div className="relative w-full p-4 max-w-md mx-auto">
+        <Button label="Cotiza aquí" onClick={() => alert('Click')} />
+        
+        <Input
+          type="tel"
+          placeholder="Celular"
+          value={phone}
+          onChange={handleInputChange}
+        >
+          <Input.Label />
+          <Input.Field pattern="[0-9]{7,12}" />
+        </Input>
+
+        <p className="mt-4 text-white">Telefono: {phone}</p>
+      </div>
     </div>
   );
 }
