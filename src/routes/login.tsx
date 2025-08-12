38ff985621f5a173088e3b37ab3cc85a02228eb4
Nelson0920 <nelson.guerrero25.ng@gmail.com>
2025-08-12 11:32:42 -0300

    created input component

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,10 +1,33 @@
-import { Button } from "../components/common";
+import { useState } from "react"
+import {
+  Button,
+  Input,
+} from "../components/common"
 
 export default function Login() {
+  const [phone, setPhone] = useState("")
+
+  const handleInputChange = (value: string) => {
+    setPhone(value)
+    console.log("Telefono:", value)
+  }
+
   return (
-    <div>
+    <div className="w-full p-4">
       <h1>Login</h1>
       <Button label="Primario" onClick={() => alert('Click')} />
+      
+      <Input
+        type="tel"
+        placeholder="Celular"
+        value={phone}
+        onChange={handleInputChange}
+      >
+        <Input.Label />
+        <Input.Field pattern="[0-9]{7,12}" />
+      </Input>
+
+      <p>Telefono: {phone}</p>
     </div>
   )
 }
